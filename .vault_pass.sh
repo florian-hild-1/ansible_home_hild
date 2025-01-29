15eeaa636ab7aa1e3164caf2277b29a33adfe8cf
Florian Hild <florian.hild@sup-logistik.de>
2025-01-29 16:29:44 +0100

    Update

--- .vault_pass.sh
+++ .vault_pass.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
 
-################################################################################
-# Kurzbeschr. : Print env variable
-# Entwickler  : F.Hild
-# Datum       : 30.01.2022
-# Ablageort   : /etc/ansible/
-# Git-Repo    : ansible.git
-# Beschreibung:
-#  ansible.cfg:
-#  vault_password_file = ./.vault_pass.sh
-#
-#  export VAULT_PASSWORD=my_vault_password
-################################################################################
-
-export LANG=C
-declare -r VERSION='1.0'
-
 echo "${VAULT_PASSWORD}"
 exit
-
